a8e2e259e913c96fbb62c3eeeb1f43e656f6f7d7
killera <zhuanqingshan@163.com>
2012-01-08 22:56:03 +0800

    set up repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDepot::Application.config.secret_token = 'ba96a429581480e4ca5e4610c06bdd2b27718704e1699283364eedbe69d3f73f8ac918c8f66bf0fa4930b2a2f90ef42529a3aca262cb3530279f40a2b1affc16'
