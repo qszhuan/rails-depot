34ff6e12cab135ac072fa3282998480bf7e10ac8
zhuanqs <zhuanqingshan@163.com>
2012-02-11 23:31:47 +0800

    create application

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,11 +1,11 @@
 class SayController < ApplicationController
   def hello
     @time = Time.now
-    Notifier.just_test.deliver
+#   Notifier.just_test.deliver
 
-    Net::SMTP.start("smtp.163.com",25,"163.com","killera","password",:login) do |smtp|  
-      smtp.send_message "sss", "killera@163.com", "killera@163.com"  
-    end
+#   Net::SMTP.start("smtp.163.com",25,"163.com","killera","password",:login) do |smtp|  
+#     smtp.send_message "sss", "killera@163.com", "killera@163.com"  
+#    end
     
   end
 
